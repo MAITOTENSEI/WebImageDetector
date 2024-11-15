f6708f9b7b3e59a07a7a2f6b74f38b113b8e04ac
MAITOTENSEI <118051779+MAITOTENSEI@users.noreply.github.com>
2024-11-15 16:21:02 +0900

    Create p5.js

---
+++ p5.js
@@ -0,0 +1,48 @@
+let capture;
+let detector,
+  detections = [];
+
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+
+  capture = createCapture({
+    video: {
+      facingMode: "environment",
+    },
+    audio: false,
+  },VIDEO);
+  capture.size(640, 640);
+  capture.hide();
+
+  detector = ml5.objectDetector("cocossd", function () {
+		alert("モデルを読み込みました!")
+    console.log("Model Loaded!");
+    detector.detect(capture, gotDetections);
+  });
+}
+
+function gotDetections(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  detections = results;
+  detector.detect(capture, gotDetections);
+}
+
+function draw() {
+  image(capture.get(), 0, 0);
+
+  for (object of detections) {
+    const { x, y, width: w, height: h, label } = object;
+
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(x, y, w, h);
+
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(label, x + 10, y + 24);
+  }
+}
